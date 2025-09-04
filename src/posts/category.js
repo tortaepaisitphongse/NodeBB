0a15d68bec66a2a711e2193bfa6a99d1ab4af241
Tor Taepaisitphongse <tortaepaisitphongse@gmail.com>
2025-09-04 15:55:27 +0000

    Refactor posts/category helpers to reduce returns in module.exports

--- src/posts/category.js
+++ src/posts/category.js
@@ -7,40 +7,43 @@ const _ = require('lodash');
 const db = require('../database');
 const topics = require('../topics');
 const activitypub = require('../activitypub');
+const winston = require('winston');
+
+async function getCidByPid(Posts, pid) {
+	winston.info('Tor Taepaisitphongse [getCidByPid called]');
+	const tid = await Posts.getPostField(pid, 'tid');
+	if (!tid && activitypub.helpers.isUri(pid)) {
+		return -1; // fediverse pseudo-category
+	}
+	return await topics.getTopicField(tid, 'cid');
+}
+
+async function getCidsByPids(Posts, pids) {
+	const postData = await Posts.getPostsFields(pids, ['tid']);
+	const tids = _.uniq(postData.map(post => post && post.tid).filter(Boolean));
+	const topicData = await topics.getTopicsFields(tids, ['cid']);
+	const tidToTopic = _.zipObject(tids, topicData);
+	const cids = postData.map(post => tidToTopic[post.tid] && tidToTopic[post.tid].cid);
+	return cids;
+}
+
+async function filterPidsBySingleCid(pids, cid) {
+	const isMembers = await db.isSortedSetMembers(`cid:${parseInt(cid, 10)}:pids`, pids);
+	return pids.filter((pid, index) => pid && isMembers[index]);
+}
+
+async function filterPidsByCid(Posts, pids, cid) {
+	if (!cid) return pids;
+	if (!Array.isArray(cid) || cid.length === 1) {
+		return await filterPidsBySingleCid(pids, cid);
+	}
+	const pidsArr = await Promise.all(cid.map(c => filterPidsByCid(Posts, pids, c)));
+	return _.union(...pidsArr);
+}
 
 module.exports = function (Posts) {
-	Posts.getCidByPid = async function (pid) {
-		const tid = await Posts.getPostField(pid, 'tid');
-		if (!tid && activitypub.helpers.isUri(pid)) {
-			return -1; // fediverse pseudo-category
-		}
-
-		return await topics.getTopicField(tid, 'cid');
-	};
-
-	Posts.getCidsByPids = async function (pids) {
-		const postData = await Posts.getPostsFields(pids, ['tid']);
-		const tids = _.uniq(postData.map(post => post && post.tid).filter(Boolean));
-		const topicData = await topics.getTopicsFields(tids, ['cid']);
-		const tidToTopic = _.zipObject(tids, topicData);
-		const cids = postData.map(post => tidToTopic[post.tid] && tidToTopic[post.tid].cid);
-		return cids;
-	};
-
-	Posts.filterPidsByCid = async function (pids, cid) {
-		if (!cid) {
-			return pids;
-		}
-
-		if (!Array.isArray(cid) || cid.length === 1) {
-			return await filterPidsBySingleCid(pids, cid);
-		}
-		const pidsArr = await Promise.all(cid.map(c => Posts.filterPidsByCid(pids, c)));
-		return _.union(...pidsArr);
-	};
-
-	async function filterPidsBySingleCid(pids, cid) {
-		const isMembers = await db.isSortedSetMembers(`cid:${parseInt(cid, 10)}:pids`, pids);
-		return pids.filter((pid, index) => pid && isMembers[index]);
-	}
+	winston.info('Tor Taepaisitphongse [init]');
+	Posts.getCidByPid = pid => getCidByPid(Posts, pid);
+	Posts.getCidsByPids = pids => getCidsByPids(Posts, pids);
+	Posts.filterPidsByCid = async (pids, cid) => filterPidsByCid(Posts, pids, cid);
 };
